49fd394da99690b299348b85534a9a9c354074b4
Daniel Rødskog <danielmero@gmail.com>
2017-09-07 18:00:30 +0200

    Rename packages and move main entrypoint

---
+++ src/main/kotlin/com/netcompany/sprak/main.kt
@@ -0,0 +1,5 @@
+package com.netcompany.sprak
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
--- src/main/kotlin/net/daniero/whatever/whatever.kt
+++
@@ -1,5 +0,0 @@
-package net.daniero.whatever
-
-fun main(args: Array<String>) {
-    println("whatever...")
-}
