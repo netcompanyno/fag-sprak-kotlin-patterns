6185941373b6e464aefbec4240efd16c06da69cd
Daniel Rødskog <danielmero@gmail.com>
2017-09-21 22:04:43 +0200

    Mock stuff with Mockito-Kotlin

--- src/test/kotlin/com/netcompany/sprak/delegator/DelegatorTest.kt
+++ src/test/kotlin/com/netcompany/sprak/delegator/DelegatorTest.kt
@@ -3,9 +3,12 @@ import com.netcompany.sprak.delegator.ABImpl
 import com.netcompany.sprak.delegator.AImpl
 import com.netcompany.sprak.delegator.BImpl
 import com.netcompany.sprak.delegator.RandomCache
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
@@ -26,16 +29,17 @@ class DelegatorTest : Spek({
 
     describe("Hidden delegate") {
         it("is initialized automatically") {
-            // TODO: mock this
-            val batchRandomizer = BatchRandomizer()
+            val batchRandomizer = mock<BatchRandomizer> {
+                on { nextBatch() } doReturn listOf(3, 1, 4, 1, 5)
+            }
 
             val foo = RandomCache(batchRandomizer)
 
-            foo.addMoreRandomNumbers()
-            // assertEquals(2, foo.get(0))
-            // assertEquals(3, foo.get(1))
-            // assertEquals(5, foo.get(2))
-            // assertEquals(7, foo.get(3))
+            assertEquals(3, foo.get(0))
+            assertEquals(1, foo.get(1))
+            assertEquals(4, foo.get(2))
+            assertEquals(1, foo.get(3))
+            assertEquals(5, foo.get(4))
         }
     }
 
