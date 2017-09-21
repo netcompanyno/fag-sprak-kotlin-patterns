0b12ce51b6aad3487d261879126337202c163288
Per Thomas Lundal <perthomas@gmail.com>
2017-09-21 16:53:48 +0200

    Adapt to iterator

--- src/main/kotlin/com/netcompany/sprak/adapter/Randomizer.kt
+++ src/main/kotlin/com/netcompany/sprak/adapter/Randomizer.kt
@@ -2,15 +2,17 @@ package com.netcompany.sprak.adapter
 
 import com.netcompany.sprak.BatchRandomizer
 
-class Randomizer(val batchRandomizer: BatchRandomizer) {
+class Randomizer(val batchRandomizer: BatchRandomizer) : Iterator<Int> {
     var batch: List<Int> = emptyList()
     var index = 0
 
-    fun next(): Int {
+    override fun next(): Int {
         if (index >= batch.size) {
             batch = batchRandomizer.nextBatch()
             index = 0
         }
         return batch[index++]
     }
+
+    override fun hasNext() = true
 }
