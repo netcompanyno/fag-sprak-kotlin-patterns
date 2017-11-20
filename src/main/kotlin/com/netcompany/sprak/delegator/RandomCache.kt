4c493fda0770afb8b63d971322b97d34dbd1b1df
Daniel Rødskog <danielmero@gmail.com>
2017-11-20 17:51:21 +0100

    Rename field and format code

--- src/main/kotlin/com/netcompany/sprak/delegator/RandomCache.kt
+++ src/main/kotlin/com/netcompany/sprak/delegator/RandomCache.kt
@@ -2,15 +2,16 @@ package com.netcompany.sprak.delegator
 
 import com.netcompany.sprak.BatchRandomizer
 
-class RandomCache private constructor(private val listDelegate: MutableList<Int>,
-                                      private val batchRandomizer: BatchRandomizer) : List<Int> by listDelegate {
+class RandomCache
+private constructor(private val cache: MutableList<Int>,
+                    private val batchRandomizer: BatchRandomizer)
+    : List<Int> by cache {
 
     constructor(batchRandomizer: BatchRandomizer) : this(ArrayList<Int>(), batchRandomizer) {
         addMoreRandomNumbers()
     }
 
     fun addMoreRandomNumbers() {
-        listDelegate.addAll(batchRandomizer.nextBatch())
+        cache.addAll(batchRandomizer.nextBatch())
     }
-
 }
