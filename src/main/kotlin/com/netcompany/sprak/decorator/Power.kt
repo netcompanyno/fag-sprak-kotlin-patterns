38f34751d59a88019ee83c43e312cbe4f534d0a1
Daniel Rødskog <danielmero@gmail.com>
2017-11-20 19:15:20 +0100

    Add missing class

--- src/main/kotlin/com/netcompany/sprak/decorator/Power.kt
+++ src/main/kotlin/com/netcompany/sprak/decorator/Power.kt
@@ -5,3 +5,9 @@ class Power(val base: Iterator<Int>) : Iterator<Int> by base {
         return 1 shl base.next()
     }
 }
+
+class Negator(private val base: Iterator<Int>) : Iterator<Int> by base {
+    override fun next(): Int {
+        return -base.next()
+    }
+}
